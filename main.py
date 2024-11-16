7cf6dfeed7bdf47c5f060b76c8c582674b922d95
Kunal Virdi <kunalvirdi.patiala@gmail.com>
2024-11-16 14:27:52 +0530

    modified main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
+from ast import main
 import pyinputplus as pyip
 from classes.index import FetchDetails, ScrapWeb, HtmlToPdf,customURL,Regex
 
+if __name__=="__main__":
+    url=pyip.inputCustom(customURL,prompt="Enter URL from Automate Boring Stuff With python website: ")
+    startingRange=pyip.inputNum("Enter starting range of heading want to get: ",min=1)
+    endingRange=pyip.inputNum("Enter ending range of heading want to get: ",min=1)
+    
+    chapterNo = Regex.chapterPattern(url)
 
+    htmlContent = FetchDetails.getHtmlContent(url)
 
-url=pyip.inputCustom(customURL,prompt="Enter URL from Automate Boring Stuff With python website: ")
-startingRange=pyip.inputNum("Enter starting range of heading want to get: ",min=1)
-endingRange=pyip.inputNum("Enter ending range of heading want to get: ",min=1)
+    scrapWeb=ScrapWeb(htmlContent=htmlContent,startingRange=startingRange,endingRange=endingRange)
+    scrapWeb.buildMapping()
+    htmlDetails=scrapWeb.getHtmlDetails()
 
-chapterNo = Regex.chapterPattern(url)
+    htmlToPdf = HtmlToPdf(chapterNo,htmlDetails)
 
-htmlContent = FetchDetails.getHtmlContent(url)
-
-scrapWeb=ScrapWeb(htmlContent=htmlContent,startingRange=startingRange,endingRange=endingRange)
-scrapWeb.buildMapping()
-htmlDetails=scrapWeb.getHtmlDetails()
-
-htmlToPdf = HtmlToPdf(chapterNo,htmlDetails)
-
-htmlToPdf.generatePdf()
+    htmlToPdf.generatePdf()
+    
+main()
 
 
 
